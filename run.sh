1a4b6b17790b035c447a7a8e7bf6aec27d1506a3
aikige <48190566+aikige@users.noreply.github.com>
2024-02-02 12:43:15 +0900

    [up] script to run test.

--- run.sh
+++ run.sh
@@ -1,1 +1,16 @@
-bundle exec jekyll serve --drafts --host 0.0.0.0 --incremental --baseurl=''
+#!/bin/sh
+BASEURL='--baseurl='
+DRAFTS=${DRAFTS:-'--drafts'}
+HOST_ADDR=${HOST:-0.0.0.0}
+PORT=${PORT:-4040}
+while [ $# -ge 1 ]; do
+	if [ $1 = "renew" ]; then
+		rm -rf _site
+	elif [ $1 = "clean" ]; then
+		DRAFTS=
+	elif [ $1 = "subdir" ]; then
+		BASEURL=
+	fi
+	shift
+done
+bundle exec jekyll serve $DRAFTS --host=$HOST_ADDR --port=$PORT --incremental $BASEURL
